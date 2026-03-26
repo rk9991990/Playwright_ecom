4a6e54528631abbb10e0316fd74eaeb18a2d60e6
Ravi Kumar <ravikumaratri97@gmail.com>
2026-03-26 17:28:11 +0530

    verify pushing code

---
+++ tests/test-pipiline.spec.js
@@ -0,0 +1,8 @@
+import { test, expect } from "@playwright/test";
+
+test("verify the title of the page", async ({ page }) => {
+  await page.goto("http://localhost:4200/");
+  await page.locator("//a[normalize-space()='About-Us']").click();
+  await expect(page).toHaveURL("http://localhost:4200/about-us");
+  await page.waitForTimeout(3000);
+});
