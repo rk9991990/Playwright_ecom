67df2f1d1605d3657f424eaf9c5e27ca99a2930c
Ravi Kumar <ravikumaratri97@gmail.com>
2026-03-28 17:01:45 +0530

    remive-hardwait

--- tests/test-pipiline.spec.js
+++ tests/test-pipiline.spec.js
@@ -4,5 +4,4 @@ test("verify the title of the page", async ({ page }) => {
   await page.goto("http://localhost:4200/");
   await page.locator("//a[normalize-space()='About-Us']").click();
   await expect(page).toHaveURL("http://localhost:4200/about-us");
-  await page.waitForTimeout(3000);
 });
